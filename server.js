d197aebd0cde519f127b16f1ab6f047edcf4e245
elfittech <elfittech@gmail.com>
2024-06-26 15:58:21 -0700

    Update server.js
    Revision to server.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var http = require('http');
+var dt = require('./timestamp');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!');
+	res.write("The date and time is currently: " + dt.myDateTime());
+	res.end('Connected');
 }).listen(8080);
 
-console.log('Connect to port 8080 through localhost');
+console.log('Connected to port 8080 through localhost');
 console.log('The result is displayed in the Command Line Interface');
-
