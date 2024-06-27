a29a7b36e357353a829161ec21e6d1cde7d3264e
elfittech <elfittech@gmail.com>
2024-06-27 00:06:26 -0700

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
 	res.write("The date and time is currently: " + dt.myDateTime());
 	res.end('Connected');
-}).listen(8080);
+}).listen(443);
 
-console.log('Connected to port 8080 through localhost');
+console.log('Connected to port(s) 80 & 443 through localhost');
 console.log('The result is displayed in the Command Line Interface');
